42f74c10849adf302711cecda9b442f870f2cc8b
kylemcland-fanduel <kyle.mcleland@fanduel.com>
2020-04-19 14:14:17 -0700

    Add game_id scraper

--- .github/README.md
+++ .github/README.md
@@ -1,3 +1,12 @@
 # college-basketball-data
 
+## Flowchart 
 ![alt text](readme_flowchart.png "Data Flowchart")
+
+## Tasks
+
+### `01-scrape_game_ids.py`
+Parameters:
+* season (int): The college basketball season to scrape. For a season e.g. 2019-2020, you would enter the year when the season ended (2020).
+
+Takes a season (int) and scrapes a list of game_ids and creates a text file with a list of all game_ids at [`/raw_data/{season}_game-ids_{run_date}.txt`](/raw_data)
---
+++ tasks/01-scrape_game_ids.py
@@ -0,0 +1,73 @@
+import os
+import re
+from datetime import date, timedelta
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_season_context(year):
+    '''
+    Return season_id, start_date, and end_date for a given year.
+    List of seasons is manually hard-coded due to small scope. 
+    To add a season, simply add it to the dictionary.
+    '''
+    seasons = {
+        2019: {
+            "season_id": 14300,
+            "start_date": date(2018, 11, 6),
+            "end_date": date(2019, 4, 8),
+        },
+        2020: {
+            "season_id": 17060,
+            "start_date": date(2019, 11, 5),
+            "end_date": date(2020, 3, 11),
+        },
+    }
+
+    if year in seasons:
+        return seasons[year]
+    else:
+        print(f"I didn't understand that input for season: {year} \n"
+              f"Please choose from the following inputs: \n"
+              f"{list(seasons)}"
+              )
+        quit()
+
+
+def game_id_scraper(season):
+    print(f"Scraping game IDs for {season}.")
+    season_context = get_season_context(season)
+
+    loop_date = season_context["start_date"]
+    loop_end_date = season_context["end_date"]
+    game_ids = []
+
+    while loop_date <= loop_end_date:
+        scoreboard_url = f"http://stats.ncaa.org/season_divisions/{season_context['season_id']}/scoreboards?game_date={loop_date.strftime('%m/%d/%y')}"
+        response = requests.get(scoreboard_url)
+
+        soup = BeautifulSoup(response.text, features="html.parser")
+        _game_ids = re.findall("contests/([0-9]*)/box_score", str(soup))
+
+        game_ids.extend(_game_ids)
+        loop_date = loop_date + timedelta(days=1)
+
+    return game_ids
+
+
+if __name__ == "__main__":
+
+    # Scrape game_ids from a season and write to .csv
+    season = 2020
+    game_ids = game_id_scraper(season=season)
+
+    date_stamp = date.today().strftime('%Y-%m-%d')
+    file_name = f"{season}_game-ids_{date_stamp}.txt"
+    file_path = os.path.join(os.path.dirname(
+        __file__), "../raw_data/", file_name)
+
+    with open(file_path, "w") as f:
+        f.write("game_id\n")  # header
+        for game_id in game_ids:
+            f.write(f"{game_id}\n")
